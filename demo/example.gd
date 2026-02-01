1203689752b551c60955c011942bedb47a8ef3f5
Lucas C. Abbas <lucasbear9@gmail.com>
2026-02-01 13:50:30 -0900

    Added haxe test

--- demo/example.gd
+++ demo/example.gd
@@ -7,3 +7,11 @@ func _ready() -> void:
 	
 	var scriptContext = QuickJSContext.new()
 	scriptContext.eval("console.log('Hello, World!')")
+	
+	var haxeJsFile = FileAccess.open("res://haxetest1/bin/main.js", FileAccess.READ)
+	var haxeJsContents = haxeJsFile.get_as_text()
+	scriptContext.eval(haxeJsContents)
+	
+	var haxeEs6JsFile = FileAccess.open("res://haxetest1/bin/main.js", FileAccess.READ)
+	var haxeEs6JsContents = haxeEs6JsFile.get_as_text()
+	scriptContext.eval(haxeEs6JsContents)
