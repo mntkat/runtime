9843ff31dfb8c1d2443fa62979af80586d87ffe0
Lucas C. Abbas <lucasbear9@gmail.com>
2026-02-04 22:26:20 -0900

    Updated example

--- demo/example.gd
+++ demo/example.gd
@@ -6,7 +6,20 @@ func _ready() -> void:
 	example.print_type(example)
 	
 	var scriptContext = QuickJSContext.new()
+	scriptContext.bind_Variant()
+	scriptContext.bind_ObjectHandle()
+	scriptContext.bind_ReferenceHandle()
 	scriptContext.eval("console.log('Hello, World!')")
+	scriptContext.eval("
+		var ref = new ReferenceHandle('res://printer.gd', 1);
+		var nameVar = VariantObject.fromString(\"Himiko\")
+		var nameStr = nameVar.toString();
+		console.log(\"Himiko\");
+		console.log(nameStr);
+		console.log(ref.isValid());
+		ref.call('helloWorld');
+		ref.call('sayHello', nameVar);
+	")
 	
 	var haxeJsFile = FileAccess.open("res://haxetest1/bin/main.js", FileAccess.READ)
 	var haxeJsContents = haxeJsFile.get_as_text()
@@ -15,3 +28,4 @@ func _ready() -> void:
 	var haxeEs6JsFile = FileAccess.open("res://haxetest1/bin/main.js", FileAccess.READ)
 	var haxeEs6JsContents = haxeEs6JsFile.get_as_text()
 	scriptContext.eval(haxeEs6JsContents)
+	
---
+++ demo/printer.gd
@@ -0,0 +1,7 @@
+extends RefCounted
+
+func sayHello(name: String):
+	print("Hello, " + name + "!")
+
+func helloWorld():
+	print("Hello from GDScript")
